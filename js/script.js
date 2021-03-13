cc8a577386845543bb2402c3d81758899767dfd1
Nazar Marko <nazar07marko@gmail.com>
2021-03-13 15:32:59 +0200

    слідкую за вчителем

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 'use strict';
 
+const box = document.getElementById('box'),
+circles = document.getElementsByClassName('circle'),
+heart = document.querySelector('.heart.first');
+console.log(heart);
+box.style.width = "500px";
